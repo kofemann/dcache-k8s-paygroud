c1960a34ca60770a6865a0abb121a26114860343
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2022-03-14 16:28:22 +0100

    grid: init proxy

--- tests/grid-test.sh
+++ tests/grid-test.sh
@@ -13,4 +13,9 @@ cp /cvmfs/grid.desy.de/etc/profile.d/scripts/glite-ui-env-old.sh /tmp/grid-ui-en
 sed -i -e '12,17d' /tmp/grid-ui-env.sh
 . /tmp/grid-ui-env.sh
 
+
+export X509_USER_PROXY=user-proxy-$CI_PIPELINE_ID
+
 arcproxy --version
+arcproxy -C usercert.pem -K userkey.pem  -T ${X509_CERT_DIR} --vomses=${VOMS_USERCONF} --vomsdir=${X509_VOMS_DIR} --voms=desy
+arcproxy -I
