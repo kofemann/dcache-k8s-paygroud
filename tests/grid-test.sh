2ee1e826f9c652c102c6fcf8461529abb3c3dfa5
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2022-03-14 15:55:19 +0100

    grid: list cvmfs directories

--- tests/grid-test.sh
+++ tests/grid-test.sh
@@ -6,6 +6,9 @@ curl https://raw.githubusercontent.com/kofemann/autoca/v1.0-py2/pyclient/autoca-
 chmod a+x autoca-client
 ./autoca-client -n -k userkey.pem -c usercert.pem ${AUTOCA_URL} "Kermit the frog"
 
+ls /cvmfs/grid.desy.de
+ls /cvmfs/grid.cern.ch
+
 # patch tu run as root
 cp /cvmfs/grid.desy.de/etc/profile.d/scripts/glite-ui-env-old.sh /tmp/grid-ui-env.sh
 sed -i -e '12,17d' /tmp/grid-ui-env.sh
