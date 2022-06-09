aa83c82362aa052f6f537c784acb8e39c4eb8820
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2022-06-09 21:53:13 +0200

    containers: add Dokerfle to build dcache container

---
+++ containers/run.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+
+if [ x$1 != x ]
+then
+  python3 /autoca-client -n -k /etc/grid-security/hostkey.pem -c /etc/grid-security/hostcer.pem ${AUTOCA_URL} $1
+fi
+
+if [ ! -f /.init_complete ]
+then
+  for f in `ls /dcache.init.d`
+  do
+    . /dcache.init.d/$f
+  done
+  touch /.init_complete
+fi
+
+systemctl daemon-reload
+systemctl list-dependencies dcache.target
+systemctl start dcache.target
+systemctl log -f  dcache@*
+
